eaa487cffd2fa4cc7900cd54d76cac136c470fa9
AmrutaDeshpande21 <91452767+AmrutaDeshpande21@users.noreply.github.com>
2023-10-19 17:46:59 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import logo from '../images/Logo .svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+    <footer>
+        <section>
+            <div className='company-info'>
+                <img src={logo} alt=''/>
+                <p>We are a family owned Mediterrean resturant focused on tradional recipes served with a modern twist</p>
+            </div>
+            <div>
+                <h3>Important Links</h3>
+                <ul>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/'>About</a></li>
+                    <li><a href='/'>Menu</a></li>
+                    <li><a href='/'>Reservation</a></li>
+                    <li><a href='/'>Order Online</a></li>
+
+                </ul>
+            </div>
+            <div>
+            <h3>Contact Us</h3>
+            <ul>
+                <li>
+                    Address: <br/> 243 street 2nd Lane Chicago USA
+                </li>
+                <li>
+                    Phone: <br/> 012 345 8765
+                </li>
+                <li>
+                    Email: <br/> LittleLemonChi@gmail.com
+                </li>
+            </ul>
+            </div>
+            <div>
+                <h3>Social Media Links</h3>
+                <ul>
+                    <li><a href="/">Facebook</a></li>
+                    <li><a href="/">Instagram</a></li>
+                    <li><a href="/">Twitter</a></li>
+
+                </ul>
+            </div>
+            </section>
+        </footer>
+
+    </div>
   )
 }
 
